59543f8d23c027cdcdf543e3cda32b3e23dc3827
agam-mishra <realagam.1997@gmail.com>
2020-05-06 00:51:02 +0530

    connecting

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,12 +4,11 @@ import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import BackgroundSection from "../components/global/BackgroundSection";
-import Info from "../components/home/info";
 const AboutPage = ({data}) => (
   <Layout>
     <SEO title="Home" keywords ={[`gatsby`,`application`, `react`]} />
     <BackgroundSection  img={data.img.childImageSharp.fluid} title="about page" styleClass='about-background'  />
-    <Info/>
+    
   </Layout>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import BackgroundSection from "../components/global/BackgroundSection";
-import Info from "../components/Home/info";
+import BackgroundSection from "../components/global/BackgroundSection"
+
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <BackgroundSection img=
     {data.img.childImageSharp.fluid} title ="FitSlash Site"></BackgroundSection>
-    <Info />
+    
   </Layout>
 );
 
