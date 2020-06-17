f2b30182ca4a4224bb871b82dc9a75b8c4a26b7d
agam-mishra <realagam.1997@gmail.com>
2020-06-17 21:07:59 +0530

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
     },
     {
 			resolve: 'gatsby-plugin-snipcart',
