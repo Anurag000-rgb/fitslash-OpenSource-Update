eeefccbc66703c337e7affa607ffd6ea6dcc6511
agam-mishra <realagam.1997@gmail.com>
2020-06-17 22:26:37 +0530

    getting it

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        spaceId: 'xpqyfc82uqen',
+        accessToken: '7sIptivTDmpwzVSrVtf-kuABnhVnBbSeuMdOxXi_DVo'
       }
     },
     {
